d84bf7d7d9846e16864b1c0f46530f921cde5a10
James <jamesabsalem@gmail.com>
2025-04-17 23:06:15 +0600

    video-224

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map, single } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -13,15 +14,24 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFatching = signal(false);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
+    this.isFatching.set(true);
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
       .subscribe({
-        next: (resData) => {
-          this.places.set(resData.places);
+        next: (response) => {
+          this.places.set(response);
+        },
+        complete: () => {
+          this.isFatching.set(false);
+        },
+        error: (error) => {
+          console.log(error);
         },
       });
 
